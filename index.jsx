45b4bca233e9c270cb20c60c5764d71872df8b07
Prom004 <izerepromise004@gmail.com>
2025-08-11 16:26:15 +0200

    new files added

--- index.jsx
+++ index.jsx
@@ -3,19 +3,25 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import Home from "./pages/Home"
 import About from "./pages/About"
+import "./server"
+import Vans from './pages/Vans';
+import VanDetail from './pages/VanDetail';
 
 function App() {
   return (
     <BrowserRouter>
       <header className='header'>
-        <Link className="site-logo font-bold" to="/">#VanLife</Link>
+          <Link className="site-logo font-bold" to="/">#VanLife</Link>
         <nav>
           <Link to="/about">About</Link>
+          <Link to="/vans">Vans</Link>
         </nav>
       </header>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/vans" element={<Vans />} />
+        <Route path="/vans/:id" element={<VanDetail />} />
       </Routes>
     </BrowserRouter>
   )
---
+++ pages/VanDetail.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default function VanDetail() {
+
+    return(
+        <h1>This is the van detail page</h1>
+    )
+}
